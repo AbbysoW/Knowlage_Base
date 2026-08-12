02015156e8bce6be4101e55723d7f7f2f73ba6fc
AbbysoW <moldovanandrew0@gmail.com>
2026-08-12 03:03:50 +0300

    added format atribute for send_request_llm

--- telegram_logic/modules/common/llm_client.py
+++ telegram_logic/modules/common/llm_client.py
@@ -1,21 +1,23 @@
 import os
+from typing import Any
 
 from dotenv import load_dotenv
 from openai import OpenAI
 
 load_dotenv()
 
-def send_request_llm(system_prompt: str, user_content: str) -> str:
+def send_request_llm(system_prompt: str, user_content: str, format: Any | None = None) -> str:
     '''
     system_prompt - системный промпт
     user_content - содержание от пользователя
+    format - json/BaseModel
     '''
     client = OpenAI(
         base_url="https://api.deepseek.com", 
         api_key=os.getenv("DEEPSEEK_API_KEY")
     )
 
-    response = client.chat.completions.create(
+    response = client.chat.completions.parse(
         model="deepseek-v4-flash",
         messages=[{
             "role": "system",
@@ -23,7 +25,8 @@ def send_request_llm(system_prompt: str, user_content: str) -> str:
         }, {
             "role": "user",
             "content": user_content
-        }]
+        }],
+        response_format=format
     )
 
     return response.choices[0].message.content
